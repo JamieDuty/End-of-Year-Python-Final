25d710b42e144f9199834a67c5b0daea3f3d36c6
JamieDuty <jamiedennisduty@gmail.com>
2025-05-01 13:31:05 -0500

    Update FinalDriver.py

--- FinalDriver.py
+++ FinalDriver.py
@@ -2,3 +2,4 @@
 #Bonuses I plan on using are randomness and puzzles
 #Do like a casino but not as much where you can gamble and go to different parts and rooms
 #games like roulette, higher or lower, etc.
+#it needs atleast 12 rooms/interactions
