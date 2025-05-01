3aeaf0c13b1e1dd9764551bd99acc1ca910101c2
JamieDuty <jamiedennisduty@gmail.com>
2025-05-01 13:27:35 -0500

    kl

---
+++ FinalClasses.py
@@ -0,0 +1,17 @@
+#This is the class program for my final project
+
+
+class A:
+    pass
+
+class B:
+    pass
+
+class C:
+    pass
+
+class D:
+    pass
+
+class E:
+    pass
--- FinalDriver.py
+++ FinalDriver.py
@@ -1,1 +1,4 @@
 #This is the driver program for my final project
+#Bonuses I plan on using are randomness and puzzles
+#Do like a casino but not as much where you can gamble and go to different parts and rooms
+#games like roulette, higher or lower, etc.
