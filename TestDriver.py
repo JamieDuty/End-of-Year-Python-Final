8157b4f7090053a0090e76d82065da3ca3baff78
JamieDuty <jamiedennisduty@gmail.com>
2025-05-13 21:52:11 -0500

    Update TestDriver.py

--- TestDriver.py
+++ TestDriver.py
@@ -2,19 +2,20 @@ from TestCode import Player
 from TestCode import Game
 
 def main():
-      name = input('What\'s your name? ')
-      player = Player(name)
-      print()
-      print(f'Welcome, {player.name}, the night is young, lets get started.')
-      player = Player(name)
-      game = Game(player)
-      game.start()
+    #basically all main does is take your name and print the first message and it calls the game and player class
+    #and the rest is run on the classes program
+    
+    #take input on player name
+    name = input('What\'s your name? ')
+    player = Player(name)
+    #print the welcome message
+    print()
+    print(f'Welcome, {player.name}, the night is young, lets get started.')
+    #start the game
+    player = Player(name)
+    game = Game(player)
+    game.start()
 if __name__ == '__main__':
-  main()
+    main()
 
-#Currently the game starts and runs, and there are 2 options and the leave option. Both Games work
-  
-#Need to improve:
-  #more options for the games and rooms
-  #Save name and dabloons to file??
-  #add secret rooms by counting up wins and if you get so many wins you get keys to secret rooms.
+
